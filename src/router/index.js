ff73678a644eacaf115d8245ec0118300ad8de39
Sebastian Zapata <sebastian.zapata@grupocnet.com>
2026-02-06 10:14:01 -0500

    update url tratamientoDatos

--- src/router/index.js
+++ src/router/index.js
@@ -6,8 +6,8 @@ const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
   routes: [
     {
-      path: "/tratamientoDatos",
-      name: "tratamientoDatos",
+      path: "/tratamientoDatosPersonales",
+      name: "tratamientoDatosPersonales",
       component: () => import("../views/TratamientoDatosView.vue"),
     },
     {
